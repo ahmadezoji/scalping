50c3507ea7f5170ab08b71477de4e00037789921
saam ezoji <saam.ezoji@portx.net>
2024-01-01 23:32:54 +0300

    update

--- bingx.py
+++ bingx.py
@@ -4,6 +4,7 @@ import requests
 import hmac
 from hashlib import sha256
 # import matplotlib.pyplot as plt
+import pandas as pd
 
 
 APIURL = "https://open-api.bingx.com"
@@ -11,7 +12,20 @@ APIKEY = 'HE9i0tkLC2oxhtjwr6F3R8VA6KMgd9gEoaedxNmiZnC9emlxdHlNkcZGltMv1U0YKPHOvT
 SECRETKEY = 'kcfaeHV6CklsCMRCZqXvC17fcnpg0dKcM6YgiwdkhCXCeB2I8GCVNkHqdlK0BWaUN7E2nFwCCUmSO2OXofA'
 
 
-def open_order(symbol, quantity):
+def positions(symbol):
+    payload = {}
+    path = '/openApi/swap/v2/user/positions'
+    method = "GET"
+    paramsMap = {
+        "recvWindow": "0",
+        "symbol": symbol,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = praseParam(paramsMap)
+    return send_request(method, path, paramsStr, payload)
+
+
+def open_long(symbol, quantity):
     payload = {}
     path = '/openApi/swap/v2/trade/order'
     method = "POST"
@@ -21,24 +35,76 @@ def open_order(symbol, quantity):
         "positionSide": "LONG",
         "type": "MARKET",
         "quantity": quantity,
+        "timestamp": str(int(time.time() * 1000))
     }
     paramsStr = praseParam(paramsMap)
     response = send_request(method, path, paramsStr, payload)
-    response.raise_for_status()
-    data = response.json().get('data', [])
-    order_info = data.get('order', {})
-    return order_info.get('orderId', None)
+    return response
 
-def close_order(symbol,order_id):
+
+def close_long(symbol, quantity):
     payload = {}
     path = '/openApi/swap/v2/trade/order'
+    method = "POST"
+    paramsMap = {
+        "symbol": symbol,
+        "side": "SELL",
+        "positionSide": "LONG",
+        "type": "MARKET",
+        "quantity": quantity,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = praseParam(paramsMap)
+    response = send_request(method, path, paramsStr, payload)
+    return response
+
+
+def close_all_order(symbol):
+    payload = {}
+    path = '/openApi/swap/v2/trade/allOpenOrders'
     method = "DELETE"
     paramsMap = {
-    "orderId": order_id,
-    "symbol": symbol,
-}
+        "recvWindow": "0",
+        "symbol": symbol,
+        "timestamp": str(int(time.time() * 1000))
+    }
     paramsStr = praseParam(paramsMap)
     return send_request(method, path, paramsStr, payload)
+
+
+def check_order_status(symbol, order_id):
+    payload = {}
+    path = '/openApi/swap/v2/trade/order'
+    method = "GET"
+
+    params_map = {
+        "symbol": symbol,
+        "orderId": order_id,
+        "timestamp": str(int(time.time() * 1000))
+    }
+
+    params_str = praseParam(params_map)
+    return send_request(method, path, params_str, payload)
+
+
+def close_order(symbol, order_id):
+    order_status_response = check_order_status(symbol, order_id)
+    print(order_status_response.json())
+    # Check if the order is still active before attempting to cancel
+    if order_status_response.json().get('status') == 'NEW':
+        # Continue with the cancel order logic
+        payload = {}
+        path = '/openApi/swap/v2/trade/order'
+        method = "DELETE"
+        params_map = {
+            "orderId": order_id,
+            "symbol": symbol,
+            "timestamp": str(int(time.time() * 1000))
+        }
+        params_str = praseParam(params_map)
+        return send_request(method, path, params_str, payload)
+    else:
+        print(f"Order {order_id} is not active and cannot be canceled.")
 
 
 def price(symbol):
@@ -52,29 +118,86 @@ def price(symbol):
     paramsStr = praseParam(paramsMap)
     return send_request(method, path, paramsStr, payload)
 
+async def get_rsi14(symbol, interval, period=14):
+    try:
+        response = get_kline(symbol, interval)
+        response.raise_for_status()
+        klines_data = response.json().get('data', [])
+        df = pd.DataFrame(klines_data, columns=['time', 'open', 'high', 'low', 'close', 'volume'])
+        df['time'] = pd.to_datetime(df['time'], unit='ms')
+        df.set_index('time', inplace=True)
+        
+        # Convert 'close' column to numeric
+        df['close'] = pd.to_numeric(df['close'], errors='coerce')
+        
+        # Drop rows with missing values in 'close' column
+        df = df.dropna(subset=['close'])
+        # Calculate price differences and handle NaN values
+        price_diff = df['close'].diff(1).fillna(0)
+        price_diff = df['close'].diff(1)
+       
+        gain = price_diff.where(price_diff > 0, 0)
+        loss = -price_diff.where(price_diff < 0, 0)
+
+        avg_gain = gain.rolling(window=period, min_periods=1).mean()
+        avg_loss = loss.rolling(window=period, min_periods=1).mean()
+
+        rs = avg_gain / avg_loss
+        rsi = 100 - (100 / (1 + rs))
+     
+
+        return rsi.iloc[-1]
+        # return 10
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error making API request: {e}")
+        return None
+
 
 async def get_rsi(symbol, interval, period=14):
     try:
         response = get_kline(symbol, interval)
         response.raise_for_status()
-        data = response.json().get('data', [])
+        klines_data = response.json().get('data', [])
 
-        if len(data) < period + 1:
-            print("Insufficient data to calculate RSI.")
+        if klines_data is None:
+            print("Error: Klines data is None.")
             return None
 
-        closes = [float(kline['close']) for kline in data]
+        if len(klines_data) < period + 1:
+            print("Insufficient data to calculate RSI.")
+            print(f"Available data: {klines_data}")
+            return None
+
+        closes = [float(kline['close']) for kline in klines_data]
 
         if len(closes) < period:
             print("Insufficient data to calculate RSI.")
+            print(f"Available closes: {closes}")
             return None
 
+        # Calculate daily price changes
         changes = [closes[i] - closes[i - 1] for i in range(1, len(closes))]
 
-        avg_gain = sum([change for change in changes if change > 0]) / period
-        avg_loss = -sum([change for change in changes if change < 0]) / period
+        # Separate gains and losses
+        gains = [change for change in changes if change > 0]
+        losses = [-change for change in changes if change < 0]
+
+        # Calculate average gains and losses for the initial period
+        avg_gain = sum(gains[:period]) / period
+        avg_loss = sum(losses[:period]) / period
+
+        # Calculate initial RS and RSI
         rs = avg_gain / avg_loss if avg_loss != 0 else float('inf')
         rsi = 100 - (100 / (1 + rs))
+
+        # Calculate RSI using SMA for the remaining data
+        for i in range(period, len(closes)):
+            change = closes[i] - closes[i - 1]
+            avg_gain = (avg_gain * (period - 1) + max(0, change)) / period
+            avg_loss = (avg_loss * (period - 1) + max(0, -change)) / period
+            rs = avg_gain / avg_loss if avg_loss != 0 else float('inf')
+            rsi = 100 - (100 / (1 + rs))
 
         return rsi
 
@@ -82,15 +205,33 @@ async def get_rsi(symbol, interval, period=14):
         print(f"Error making API request: {e}")
         return None
 
+
+def get_server_time():
+    payload = {}
+    path = '/openApi/swap/v2/server/time'
+    method = "GET"
+    paramsMap = {
+    }
+    paramsStr = praseParam(paramsMap)
+    response = send_request(method, path, paramsStr, payload)
+    response.raise_for_status()
+    data = response.json().get('data', [])
+    return data.get('serverTime', {})
+
+
 def get_kline(symbol, interval, period=14):
+    server_time = get_server_time()
     payload = {}
     path = '/openApi/swap/v3/quote/klines'
     method = "GET"
     paramsMap = {
         "symbol": symbol,
         "interval": interval,
-        "limit": period + 1,
+        "limit": 10,
+        "timestamp": str(int(time.time() * 1000))
     }
+    
+
     paramsStr = praseParam(paramsMap)
     return send_request(method, path, paramsStr, payload)
 
@@ -150,5 +291,5 @@ def praseParam(paramsMap):
 #         plt.show()
 
 
-if __name__ == '__main__':
-    showKlines(symbol="BTC-USDT", interval="1m")
+# if __name__ == '__main__':
+#     showKlines(symbol="BTC-USDT", interval="1m")
--- scalping.py
+++ scalping.py
@@ -12,28 +12,35 @@ async def scalping_strategy(symbol, interval):
     global order_status_open
     while True:
         try:
-            rsi = await get_rsi(symbol, interval)
+            rsi = await get_rsi14(symbol, interval)
             print(f'Current RSI for {interval} interval: {rsi}')
-            
-            if not order_status_open and rsi < 30:
-                print('RSI is below 30. Initiating buy order...')
-                current_order_id  = open_order(symbol="BTC-USDT",quantity=quantity)
-                if current_order_id is not None:
-                    order_status_open = True
 
-            elif order_status_open and rsi > 70:
-                print('RSI is above 70. Initiating sell order...')
-                close_order(symbol=symbol,order_id=current_order_id)
-                order_status_open = False
+            # if not order_status_open and rsi < 30:
+            #     print('RSI is below 30. Initiating buy order...')
+            #     response = open_long(symbol=symbol, quantity=quantity)
+            #     print(f'Buy order executed: {response.text}')
+            #     response.raise_for_status()
+            #     data = response.json().get('data', [])
+            #     order_info = data.get('order', {})
+            #     current_order_id = order_info.get('orderId', None)
+            #     if current_order_id is not None:
+            #         order_status_open = True
+
+            # elif order_status_open and rsi > 70:
+            #     print('RSI is above 70. Initiating sell order...')
+            #     close_long(symbol=symbol, quantity=quantity)
+            #     print(f'Sell order executed: {response.text}')
+            #     order_status_open = False
 
             await asyncio.sleep(5)
 
         except Exception as e:
             print(f'An error occurred: {e}')
 
+
 async def main():
     symbol = "BTC-USDT"
-    interval = "1m"
+    interval = "1w"
     await asyncio.gather(
         scalping_strategy(symbol, interval),
         # Add more strategies here if needed
@@ -41,6 +48,4 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-    
-    
-   
+
