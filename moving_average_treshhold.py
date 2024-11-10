6cd91615885e066f7e8e57a37384803bcc5def7d
saam ezoji <saam.ezoji@portx.net>
2024-11-10 15:31:08 +0300

    update

--- moving_average_treshhold.py
+++ moving_average_treshhold.py
@@ -17,14 +17,14 @@ MIN = 1
 LIMIT = 100
 AMOUNT_USDT = 2000  # USDT
 
-SL = -0.05 # % stop loss percentage
-TP = 0.15  # % take profit percentage
+SL = -0.04 # % stop loss percentage
+TP = 0.12  # % take profit percentage
 
 # THRESHOLD_PERCENTAGE = 0.002 # Moderate Sensitivity
 # THRESHOLD_PERCENTAGE = 0.001 # High Sensitivity
 # THRESHOLD_PERCENTAGE = 0.0005 # Very High Sensitivity
 # THRESHOLD_PERCENTAGE = 0.0001  # ultra High Sensitivity
-THRESHOLD_PERCENTAGE = 0.0001
+THRESHOLD_PERCENTAGE = 0.001
 ATR_PERIOD = 14  # ATR period
 
 order_type = OrderType.NONE
@@ -211,7 +211,7 @@ async def main():
 
 
 # Backtesting function for the past two days with plotting
-def backtest(symbol, interval="1m", days=2):
+def backtest(symbol, interval="1m", days=1):
     global order_type, ordered_price, last_order_id, count_of_long, count_of_short
 
     # Resetting trading state for backtest
@@ -222,7 +222,7 @@ def backtest(symbol, interval="1m", days=2):
     last_trade_amount = None
     
     # Fetch data for the past two days (2 * 24 * 60 / 1-minute interval)
-    limit = days * 12 * 60  # Number of minutes in 2 days
+    limit = days * 10 * 60  # Number of minutes in 2 days
     past_time_ms = get_server_time() - (limit * 60 * 1000)
     
     klines = get_kline(symbol=symbol, interval=interval, limit=limit, start=past_time_ms)
@@ -309,54 +309,54 @@ def backtest(symbol, interval="1m", days=2):
     plt.grid(True)
     plt.show()
     # Function to calculate moving averages for backtesting
-    def calculate_all_smas(klines):
-        close_prices = [float(kline['close']) for kline in klines]
-        sma5 = [sum(close_prices[i:i+5]) /
-                5 for i in range(len(close_prices)-5+1)]
-        sma8 = [sum(close_prices[i:i+8]) /
-                8 for i in range(len(close_prices)-8+1)]
-        sma13 = [sum(close_prices[i:i+13]) /
-                 13 for i in range(len(close_prices)-13+1)]
-        return close_prices, sma5, sma8, sma13
+    # def calculate_all_smas(klines):
+    #     close_prices = [float(kline['close']) for kline in klines]
+    #     sma5 = [sum(close_prices[i:i+5]) /
+    #             5 for i in range(len(close_prices)-5+1)]
+    #     sma8 = [sum(close_prices[i:i+8]) /
+    #             8 for i in range(len(close_prices)-8+1)]
+    #     sma13 = [sum(close_prices[i:i+13]) /
+    #              13 for i in range(len(close_prices)-13+1)]
+    #     return close_prices, sma5, sma8, sma13
 
-    last_min = 180
-    past_time_ms = get_server_time() - (last_min * 60 * 1000)
-    klines = get_kline(symbol=symbol, interval=interval,
-                       limit=last_min, start=past_time_ms)
-    klines.raise_for_status()
-    klines = klines.json().get('data', [])
-    klines.reverse()
+    
+    # past_time_ms = get_server_time() - (limit * 60 * 1000)
+    # klines = get_kline(symbol=symbol, interval=interval,
+    #                    limit=limit, start=past_time_ms)
+    # klines.raise_for_status()
+    # klines = klines.json().get('data', [])
+    # klines.reverse()
 
-    if not klines:
-        logger.error("No data available for backtesting.")
-        return
+    # if not klines:
+    #     logger.error("No data available for backtesting.")
+    #     return
 
-    close_prices, sma5, sma8, sma13 = calculate_all_smas(klines)
+    # close_prices, sma5, sma8, sma13 = calculate_all_smas(klines)
 
-    # Plotting the results
-    plt.figure(figsize=(14, 7))
-    plt.plot(close_prices, label="Close Price", color="black")
+    # # Plotting the results
+    # plt.figure(figsize=(14, 7))
+    # plt.plot(close_prices, label="Close Price", color="black")
 
-    # Align SMA arrays to the corresponding close price indices
-    plt.plot(range(4, len(close_prices)), sma5, label="SMA5", color="brown")
-    plt.plot(range(7, len(close_prices)), sma8, label="SMA8", color="orange")
-    plt.plot(range(12, len(close_prices)), sma13, label="SMA13", color="blue")
+    # # Align SMA arrays to the corresponding close price indices
+    # plt.plot(range(4, len(close_prices)), sma5, label="SMA5", color="brown")
+    # plt.plot(range(7, len(close_prices)), sma8, label="SMA8", color="orange")
+    # plt.plot(range(12, len(close_prices)), sma13, label="SMA13", color="blue")
 
-    # Formatting the plot
-    plt.title(f"{symbol} Backtest - Close Prices and SMAs")
-    plt.xlabel("Time (most recent to older)")
-    plt.ylabel("Price")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # # Formatting the plot
+    # plt.title(f"{symbol} Backtest - Close Prices and SMAs")
+    # plt.xlabel("Time (most recent to older)")
+    # plt.ylabel("Price")
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
 
 
 # Run the async loop
 if __name__ == "__main__":
     try:
         # asyncio.run(back_test(symbol=SYMBOL))
-        asyncio.run(main())
-        # asyncio.run(backtest(symbol=SYMBOL))
+        # asyncio.run(main())
+        asyncio.run(backtest(symbol=SYMBOL))
 
     except KeyboardInterrupt:
         logger.info("Trading bot stopped manually.")
